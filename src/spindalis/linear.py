786f878f81cbfa0f801b9afa867f3a1ab4edf315
Andrew Hennis <dawnandrew100@gmail.com>
2025-05-18 21:08:04 -0400

    Getting Rusty
    Adding Rust version of functions and also turned python variance and
    covariance functions into sample variance instead of population variance

--- src/spindalis/linear.py
+++ src/spindalis/linear.py
@@ -16,10 +16,10 @@ class PCA():
         :param dimension_data: list[list[int]] | list[list[float]] 2D data matrix
         :return: list[list[float]] centered data
 
-        Takes the 2D array of different dimensional data and centers the data around the origin of the graph 
+        Takes the 2D array of different dimensional data and centers the data around the origin of the graph
         by subtracting the mean from each point.
         """
-        if not all(all(isinstance(value, (int, float)) for value in dimension) for dimension in dimension_data): 
+        if not all(all(isinstance(value, (int, float)) for value in dimension) for dimension in dimension_data):
             raise TypeError(self.not_a_num)
         new_data = []
         for dimension in dimension_data:
@@ -35,10 +35,10 @@ class PCA():
         Takes a single dimension of data and calculates the variance of all of the points within that dimension.
         Note: the covariance of the same dimension is the same as the variance of that dimension.
         """
-        if not all(isinstance(value, (int, float)) for value in dimension_data): 
+        if not all(isinstance(value, (int, float)) for value in dimension_data):
             raise TypeError(self.not_a_num)
         mean = sum(dimension_data)/len(dimension_data)
-        return sum(map(lambda x: (x - mean)**2, dimension_data))/len(dimension_data)
+        return sum(map(lambda x: (x - mean)**2, dimension_data))/(len(dimension_data) - 1)
 
     def covariance(self, x_data: list[int] | list[float], y_data: list[int] | list[float]) -> float:
         """
@@ -54,7 +54,7 @@ class PCA():
         if not all(isinstance(value, (int, float)) for value in x_data) or not all(isinstance(value, (int, float)) for value in y_data):
             raise TypeError(self.not_a_num)
         x_mean, y_mean = sum(x_data)/len(x_data), sum(y_data)/len(y_data)
-        return sum(map(lambda x, y: ((x-x_mean)*(y-y_mean)), x_data, y_data))/(len(x_data))
+        return sum(map(lambda x, y: ((x-x_mean)*(y-y_mean)), x_data, y_data))/(len(x_data) - 1)
 
     def cov_mat(self, dimension_data: list[list[int]] | list[list[float]], rounded: bool = False, digits: int = 2) -> list[list[float]]:
         """
@@ -65,7 +65,7 @@ class PCA():
 
         Applies covariance function in order to produce a matrix of the covariance for each dimension of data.
         """
-        if not all(all(isinstance(value, (int, float)) for value in dimension) for dimension in dimension_data): 
+        if not all(all(isinstance(value, (int, float)) for value in dimension) for dimension in dimension_data):
             raise TypeError(self.not_a_num)
         if rounded:
             return [[round(self.covariance(x, y), digits) for y in dimension_data] for x in dimension_data]
@@ -77,12 +77,12 @@ class PCA():
         :return: list[list[int]] | list[list[float]] 2D data matrix
 
         Takes a 2D array as an input and reflects it on the diagonal axis.
-        Intended to be used so that all rows contain data of the same type 
+        Intended to be used so that all rows contain data of the same type
         (ex. first row is water temperature data).
         """
         rows, cols = len(matrix), len(matrix[0])
         transposed = [[0 for _ in range(rows)] for _ in range(cols)]
-        
+
         for i in range(rows):
             for j in range(cols):
                 transposed[j][i] = matrix[i][j]
