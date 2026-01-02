0b8653dcd832e7396e7ba5defe0bbbf1ec80c377
= <=>
2026-01-02 11:48:31 +0100

    feat: Added procedural macro for definite integral

--- spindalis_macros/src/lib.rs
+++ spindalis_macros/src/lib.rs
@@ -1,5 +1,7 @@
 use proc_macro::TokenStream;
 use std::str::FromStr;
+use quote::quote;
+use syn::{parse_macro_input, LitStr, LitFloat, Token, parse::Parse, parse::ParseStream, Result};
 
 // Polynomial Parsing Macros
 
@@ -55,8 +57,58 @@ pub fn parse_polynomial_extended(input: TokenStream) -> TokenStream {
 }
 
 
+
+
+
+
+
+struct IntegralArgs {
+    poly_str: String,
+    lower: f64,
+    upper: f64,
+}
+
+// Parses: ("3x^2 + 2", 0.0, 1.0)
+impl Parse for IntegralArgs {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let poly_str: LitStr = input.parse()?;
+        input.parse::<Token![,]>()?;
+        let lower: LitFloat = input.parse()?;
+        input.parse::<Token![,]>()?;
+        let upper: LitFloat = input.parse()?;
+        
+        Ok(IntegralArgs {
+            poly_str: poly_str.value(),
+            lower: lower.base10_parse()?,
+            upper: upper.base10_parse()?,
+        })
+    }
+}
+
 #[proc_macro]
-pub fn parse_definite_integral(input : TokenStream) -> TokenStream {
-    let output = parse_simple_polynomial!(input);
-    
+pub fn definite_integral(input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(input as IntegralArgs);
+
+    //  Parse string to coefficients using your existing core logic
+    let coefficients = match spindalis_core::polynomials::simple::parse_simple_polynomial(args.poly_str) {
+        Ok(coeffs) => coeffs.coefficients,
+        Err(e) => {
+            let err = format!("Compile-time Polynomial Parse Error: {:?}", e);
+            return quote!(compile_error!(#err)).into();
+        }
+    };
+
+    //  Calculate the result using your analytical_integral function
+    let result = spindalis_core::integrals::analytical_integral(
+        &coefficients, 
+        args.lower, 
+        args.upper
+    );
+
+    //  Emit the final result as a float literal
+    let expanded = quote! {
+        #result
+    };
+
+    TokenStream::from(expanded)
 }
